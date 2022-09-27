f09a08465013089ffce9e87dbe8d82176bcf44dc
Qurban Ahmad <ahmad76qurban@gmail.com>
2022-09-27 18:26:43 +0530

    feat: copy jwt token generation code

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { sign } from "jsonwebtoken";
+import { randomUUID } from "crypto";
+
+function getJWTToken(secretID: string, clientID: string, userName: string) {
+  const token = sign(
+    {
+      /**
+       * Added scopes that were present in the reference documentation.
+       * Don't know much about what permissions they allow.
+       */
+      scope: [
+        "tableau:views:embed",
+        "tableau:metrics:embed",
+        "tableau:metrics:embed",
+      ],
+    },
+    secretID,
+    {
+      issuer: clientID, // Tableau require it in header, but there is no attribute in header for 'issuer', so added here.
+      subject: userName, // Username in Tableau
+      audience: "tableau", // must be 'tableau'
+      jwtid: randomUUID(), // The JWT ID claim provides a unique identifier for the JWT
+      expiresIn: 3600, // 1 hour
+      header: {
+        alg: "HS256",
+        kid: secretID, // Tableau require it in header. Ref: https://help.tableau.com/current/online/en-us/connected_apps.htm
+      },
+    }
+  );
+  return token;
+}
+
+getJWTToken("", "", "");
