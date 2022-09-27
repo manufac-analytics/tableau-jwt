7ab16d45582c1b5e30421756033430bf8d1b65a4
Maneet Goyal <maneetgoyal14@gmail.com>
2022-09-27 21:38:45 +0530

    Add comments

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,12 @@ export function getTableauToken(userName: string) {
   };
   const token = sign(
     {
-      scp: ["tableau:views:embed", "tableau:metrics:embed"],
+      scp: ["tableau:views:embed", "tableau:metrics:embed"], // scope
     },
     process.env.CONNECTED_APP_SECRET_VALUE ?? "",
     {
       audience: "tableau", // must be 'tableau'
-      subject: userName, // Username in Tableau
+      subject: userName, // Username as per Tableau platform
       issuer: process.env.CONNECTED_APP_CLIENT_ID, // Tableau require it in header, but there is no attribute in header for 'issuer', so added here.
       jwtid: randomUUID(), // The JWT ID claim provides a unique identifier for the JWT
       expiresIn: 600, // 10 minutes (max limit set by Tableau)
