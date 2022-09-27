fa734656100d21d60e62661f47afb14e9a5e03f0
Maneet Goyal <maneetgoyal14@gmail.com>
2022-09-27 19:41:56 +0530

    Update signing logic

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
 import { sign } from "jsonwebtoken";
+import type { JwtHeader } from "jsonwebtoken";
 import { randomUUID } from "crypto";
 
 function getJWTToken(userName: string) {
-  const clientID = "";
-  const secretID = "";
+  const connectedAppClientId = "clientID";
+  const connectedAppSecretId = "secretID";
+  const connectedAppSecretValue = "secretValue";
+  
+  const header: JwtHeader = {
+    alg: "HS256",
+    kid: connectedAppSecretId, // Tableau require it in header. Ref: https://help.tableau.com/current/online/en-us/connected_apps.htm
+    // @ts-expect-error `iss` is needed as per Tableau docs but there is type mismatch as per `jsonwebtoken` library
+    iss: connectedAppClientId
+  };
   const token = sign(
     {
-      scope: ["tableau:views:embed", "tableau:metrics:embed"],
+      scp: ["tableau:views:embed", "tableau:metrics:embed"],
     },
-    secretID,
+    connectedAppSecretValue,
     {
-      issuer: clientID, // Tableau require it in header, but there is no attribute in header for 'issuer', so added here.
-      subject: userName, // Username in Tableau
       audience: "tableau", // must be 'tableau'
+      subject: userName, // Username in Tableau
+      issuer: connectedAppClientId, // Tableau require it in header, but there is no attribute in header for 'issuer', so added here.
       jwtid: randomUUID(), // The JWT ID claim provides a unique identifier for the JWT
       expiresIn: 3600, // 1 hour
-      header: {
-        alg: "HS256",
-        kid: secretID, // Tableau require it in header. Ref: https://help.tableau.com/current/online/en-us/connected_apps.htm
-      },
+      header,
     }
   );
   return token;
 }
 
-getJWTToken("");
+console.log(getJWTToken(""));
