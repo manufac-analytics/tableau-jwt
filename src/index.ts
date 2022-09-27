6f34bc2fe10f263f84e13c9659ffaa59c138d80c
Maneet Goyal <maneetgoyal14@gmail.com>
2022-09-27 20:42:22 +0530

    Add env variables

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import { sign } from "jsonwebtoken";
 import type { JwtHeader } from "jsonwebtoken";
 import { randomUUID } from "crypto";
 
-function getJWTToken(userName: string) {
-  const connectedAppClientId = "clientID";
-  const connectedAppSecretId = "secretID";
-  const connectedAppSecretValue = "secretValue";
-  
+// ENV Variables
+const connectedAppClientId = "69cbbd39-32f4-4035-848e-2d22a9c349a0";
+const connectedAppSecretId = "7d4ffeb3-6ff9-4e56-9e6e-963018f0f291";
+const connectedAppSecretValue = "sDpIcz8HLOQ3Ou+HmQ9oo53BxyUsJmQkdrwk0H/+UWU=";
+
+/**
+ * How to encode? https://help.tableau.com/current/online/en-us/connected_apps.htm#jwt
+ * How to troubleshoot? https://help.tableau.com/current/online/en-us/connected_apps_troubleshoot.htm
+ * @param userName 
+ * @returns 
+ */
+function getJWTToken(userName: string) {  
   const header: JwtHeader = {
     alg: "HS256",
     kid: connectedAppSecretId, // Tableau require it in header. Ref: https://help.tableau.com/current/online/en-us/connected_apps.htm
@@ -23,11 +30,11 @@ function getJWTToken(userName: string) {
       subject: userName, // Username in Tableau
       issuer: connectedAppClientId, // Tableau require it in header, but there is no attribute in header for 'issuer', so added here.
       jwtid: randomUUID(), // The JWT ID claim provides a unique identifier for the JWT
-      expiresIn: 3600, // 1 hour
+      expiresIn: 600, // 10 minutes (max limit set by Tableau)
       header,
     }
   );
   return token;
 }
 
-console.log(getJWTToken(""));
+console.log(getJWTToken("mgoyal@manufacanalytics.com"));
